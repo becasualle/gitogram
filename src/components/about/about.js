86a9b7f3abd138c17da8e833b408100f9ef8a4f5
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-01-14 19:50:58 +0300

    deploy setup

---
+++ src/components/about/about.js
@@ -0,0 +1,6 @@
+export default {
+    name: "about",
+    props: {
+      msg: String,
+    },
+}
---
+++ src/components/about/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h1>Привет, курс!</h1>
+  </div>
+</template>
+
+<script src="./about.js">
+</script>
+
+<style lang='scss' scoped>
+h1 {
+    color: crimson;
+}
+</style>
